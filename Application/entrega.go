0ea8d15548345cecc69eaafd7ddd5543ea55c779
Laurindo <laurindohenriquesmoises33@gmail.com>
2024-05-16 09:22:04 +0100

    Entrega Actualizada

--- Application/entrega.go
+++ Application/entrega.go
@@ -7,17 +7,19 @@ type PedidoEntrega struct{}
 func (e *PedidoEntrega) FormalizarPedido(cliente *model.Cliente) bool {
 	cliente.Encomenda.PagamentoFormalizado = true
 
-	return cliente.Encomenda.PagamentoFormalizado
+	return true
 
 }
 
 func (e *PedidoEntrega) ValidarPagamento(cliente *model.Cliente) bool {
 	cliente.Encomenda.PagamentoFormalizado = true
-	return cliente.Encomenda.PagamentoFormalizado
+	return true
 }
 
 func (e *PedidoEntrega) VericarDestinatario(cliente *model.Cliente) string {
+	
 	cliente.Destinatario.Morada = "Luanda"
+	morada:=cliente.Destinatario.Morada
 
-	return cliente.Destinatario.Morada
+	return morada
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module fintregas
 
-go 1.20
+go 1.22.2
+
+require github.com/google/uuid v1.6.0 // indirect
